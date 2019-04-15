8b18795c0be616a1c7c1da695cb47c18328e4a5e
Saurav Mishra <Saurav.Mishra@NIIT-Tech.com>
2019-04-15 14:43:16 +0530

    Add project files.

---
+++ CoreApparelManagerPortal/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreApparelManagerPortal.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApparelManagerPortal.Controllers
+{
+    [Route("Account")]
+    public class AccountController : Controller
+    {
+        OnlineApparelStoreDbContext context;
+        public AccountController(OnlineApparelStoreDbContext _context)
+        {
+            context = _context;
+        }
+        [Route("")]
+        [Route("index")]
+        [Route("~/")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("login")]
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            Managers ad = context.Managers.Where(x => x.ManagerEmail == username).SingleOrDefault();
+            if (username != null && password != null && ad != null && password.Equals(ad.ManagerPassword))
+            {
+                HttpContext.Session.SetString("uname", ad.ManagerFirstName + " " + ad.ManagerLastName);
+                return View("Home");
+            }
+            else
+            {
+                ViewBag.Error = "Invalid Credentials";
+                return View("Index");
+            }
+
+        }
+        [Route("logout")]
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("uname");
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CoreApparelManagerPortal/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreApparelManagerPortal.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreApparelManagerPortal
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            services.AddSession();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<OnlineApparelStoreDbContext>(item => item.UseSqlServer(Configuration.GetConnectionString("DefaultStrings")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Account}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
